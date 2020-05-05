89c513242d6a3f8d43d349866c19e12ac4b23635
senthusj <senthu231997@gmail.com>
2020-05-05 20:27:09 +0530

    Database connection

--- HospitalManagement/src/com/hospital.java
+++ HospitalManagement/src/com/hospital.java
@@ -1,5 +1,19 @@
 package com;
 
+import java.sql.*;
+
 public class hospital {
 
+	private Connection connect() {
+		Connection con = null;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			// Provide the correct details: DBServer/DBName, username, password
+			con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/hospital", "root", "");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return con;
+	}
+	
 }
